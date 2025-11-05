6c2d4bdb9d90163b5b18a1f12552aa79b865be77
Thiekshana Perera <thiekshana@phyxle.com>
2025-11-05 11:50:56 +0530

    update ignore

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ import os
 
 
 ROOT_DIR = Path(__file__).parent
-load_dotenv(ROOT_DIR / '.env')
+# load_dotenv(ROOT_DIR / '.env') 
+load_dotenv(ROOT_DIR / '.env.production') 
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
